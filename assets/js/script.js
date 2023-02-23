de29dc628e32ec6b089c8eeccb155dac11ddbcf8
CBurton <burton.chris13@gmail.com>
2023-02-23 17:42:17 -0600

    Updated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
-var numQuotes = 110;
+var genBtn = document.getElementById('genButton');
+var quoteSection = document.querySelector("#quote");
+var numQuotes = 2; //110
 var QuoteID = 0;
 
 // randomly pickes quote
 var getQuote = function() {
     var randomQuote =Math.floor(Math.random() * numQuotes)+ 1;
     QuoteID = randomQuote;
+};
+
+var genQuote = function(){
+    getQuote();
+    console.log(QuoteID);
+    
+    quoteSection.append('<br>' + QuoteID + '<br>')
 }
+
+
+
+genBtn.addEventListener("click", function(){
+    genQuote();
+})
