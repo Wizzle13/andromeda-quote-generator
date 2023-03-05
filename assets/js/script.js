03305a6bc30aded2e6ff1818334bdf5f217a1d7a
CBurton <burton.chris13@gmail.com>
2023-03-05 14:33:36 -0600

    displays new quote on screen.  doesnot clear screen yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,37 @@ var genBtn = document.getElementById('genButton');
 var quoteSection = document.querySelector("#quote");
 var numQuotes = 2; //110
 var QuoteID = 0;
-
+var modalQuote = document.getElementById("quote");
 // randomly pickes quote
 var getQuote = function() {
-    var randomQuote =Math.floor(Math.random() * numQuotes)+ 1;
+    var randomQuote =Math.floor(Math.random() * numQuotes);
     QuoteID = randomQuote;
 };
 
 var genQuote = function(){
     getQuote();
-    console.log(QuoteID);
-    
-    quoteSection.append('<br>' + QuoteID + '<br>')
+    modalQuote.style.display = "block";
+    const apiUrl = "./assets/api/quotes.json" ;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            
+            console.log(data); // do something with the data here
+            const Rquote = data.quotes[QuoteID];
+
+            // extract the quote and name properties
+            const { Rquote: quoteText, name, source,year, quote } = Rquote;
+             
+
+            // document.querySelector('intro').style.visibility='hidden';
+            $(quoteSection).append(
+                "<div><br>" + `"${quote}"` + "<br>" + `${name}` + "<br>" + `${source}` + "<br>" +`${year}` + "<br></div>"
+                );
+        })
+        .catch(error => {
+        console.error('Error fetching JSON data:', error);
+        
+    });
 }
 
 
